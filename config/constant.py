c45dba4219c4d788555e48c44f6dcb629c83428d
Priscillia Ejiro <ejirogoro27@gmail.com>
2026-05-21 12:10:25 +0100

    config update

--- config/constant.py
+++ config/constant.py
@@ -1,21 +1,6 @@
 import os
 from dotenv import load_dotenv
 
-load_dotenv(override = True)
-
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
-
-
-apex_model_name = "Apex_Trust_model"
-
-
-
-
-
-import os
-from dotenv import load_dotenv
-
 load_dotenv(override=True)
 
 # =========================
